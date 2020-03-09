4d4eef3883b1bc292f3b0a7618c8784c75d5dddb
shotasan <wondermoai@gmail.com>
2020-03-09 23:00:52 +0900

    array.ts

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+// 非推奨の書き方
+let numbers2: Array<number> = [1, 2, 3];
+let string2: Array<string> = ['tokyo', 'osaka', 'kyoto'];
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenHairetu: number[][] = [
+  [50, 100],
+  [150, 300]
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, 'japan'];
